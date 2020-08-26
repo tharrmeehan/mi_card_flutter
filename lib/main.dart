e430acb7a369daae792c22bac658ad1c7e020b02
tharrmeehan <58373200+tharrmeehan@users.noreply.github.com>
2020-08-26 20:06:40 +0200

    Testing

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,25 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
+        appBar: AppBar(
+          title: Center(child: Text("Purity")),
+          backgroundColor: Color(0xff0000000),
+        ),
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: 
+          Center(child: Text("Hello")),
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: Color(0xff000000),
+          child: Icon(Icons.add)),
       ),
-    ),
-  );
+    );
+  }
 }
